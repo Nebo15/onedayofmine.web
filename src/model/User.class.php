c192d992fa94747c34ac86d7034e431d9f1291a6
korchasa <korchasa@gmail.com>
2012-05-31 21:14:46 +0300

    Swagger stuff removed

--- src/model/User.class.php
+++ src/model/User.class.php
@@ -1,13 +1,6 @@
 <?php
 lmb_require('src/model/BaseModel.class.php');
 
-/**
- *@SwaggerModel(
- *     id="user",
- *     description="some long description of the model"
- * )
- * @property integer $id
- */
 class User extends lmbActiveRecord
 {
 	static function findByFbIdAndToken($fb_user_id, $fb_access_token)
