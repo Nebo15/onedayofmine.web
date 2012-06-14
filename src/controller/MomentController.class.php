4f0ccd48a56a1d6d006c4c89d9d6f0f106d1bacc
korchasa <korchasa@gmail.com>
2012-06-14 16:54:34 +0300

    Moment mock implementation

--- src/controller/MomentController.class.php
+++ src/controller/MomentController.class.php
@@ -1,7 +1,28 @@
 <?php
-lmb_require('limb/cms/src/controller/lmbObjectController.class.php');
+lmb_require('src/controller/BaseJsonController.class.php');
 lmb_require('src/model/Moment.class.php');
-class MomentController extends lmbObjectController
+
+class MomentController extends BaseJsonController
 {
   protected $_object_class_name = 'Moment';
+
+  function doUpdate()
+  {
+    return $this->_answerOk(odMock::moment());
+  }
+
+  function doShare()
+  {
+    return $this->_answerOk();
+  }
+
+  function doComment()
+  {
+    return $this->_answerOk(array(odMock::momentComment(), odMock::momentComment()));
+  }
+
+  function doDelete()
+  {
+    return $this->_answerOk();
+  }
 }
--- tests/cases/acceptance/MomentAcceptanceTest.class.php
+++ tests/cases/acceptance/MomentAcceptanceTest.class.php
@@ -4,26 +4,42 @@ lmb_require('tests/cases/odAcceptanceTestCase.class.php');
 
 class MomentAcceptanceTest extends odAcceptanceTestCase
 {
+  /**
+   *@example
+   */
   function testUpdate()
   {
+    $this->_loginAndSetCookie($this->main_user);
     $this->post('moment/update');
     $this->assertResponse(200);
   }
 
+  /**
+   *@example
+   */
   function testDelete()
   {
+    $this->_loginAndSetCookie($this->main_user);
     $this->post('moment/delete');
     $this->assertResponse(200);
   }
 
+  /**
+   *@example
+   */
   function testComment()
   {
+    $this->_loginAndSetCookie($this->main_user);
     $this->post('moment/comment');
     $this->assertResponse(200);
   }
 
+  /**
+   *@example
+   */
   function testShare()
   {
+    $this->_loginAndSetCookie($this->main_user);
     $this->post('moment/share');
     $this->assertResponse(200);
   }
