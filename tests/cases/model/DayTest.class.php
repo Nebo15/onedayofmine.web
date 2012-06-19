45c8f44905e4cd91cb6abeb64121ca5aef4a7d23
korchasa <korchasa@gmail.com>
2012-06-19 18:29:31 +0300

    Validation test for Day

---
+++ tests/cases/model/DayTest.class.php
@@ -0,0 +1,34 @@
+<?php
+
+lmb_require('tests/cases/odUnitTestCase.class.php');
+
+class DayTest extends odUnitTestCase
+{
+  function testValidator()
+  {
+    $this->assertTrue($this->_createValidDay()->validate());
+
+    $day = $this->_createValidDay();
+    $day->setTitle(null);
+    $this->assertFalse($day->validate());
+
+    $day = $this->_createValidDay();
+    $day->setDescription(null);
+    $this->assertFalse($day->validate());
+
+    $day = $this->_createValidDay();
+    $day->setUser(null);
+    $this->assertFalse($day->validate());
+
+    $day = $this->_createValidDay();
+    $day->setUser(new Day());
+    $this->assertFalse($day->validate());
+  }
+
+  protected function _createValidDay()
+  {
+    $user = $this->generator->user();
+    $user->save();
+    return $this->generator->day($user);
+  }
+}
