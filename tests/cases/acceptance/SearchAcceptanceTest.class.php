bd2025ea600228c0584c66787aba2e363ee9b793
korchasa <korchasa@gmail.com>
2012-06-29 22:18:50 +0300

    search/suggest removed

--- tests/cases/acceptance/SearchAcceptanceTest.class.php
+++ tests/cases/acceptance/SearchAcceptanceTest.class.php
@@ -7,15 +7,6 @@ class SearchAcceptanceTest extends odAcceptanceTestCase
   /**
    *@example
    */
-  function testSuggest()
-  {
-    $res = $this->get('search/suggest');
-    $this->assertResponse(200);
-  }
-
-  /**
-   *@example
-   */
   function testSearch()
   {
     $res = $this->get('search/text');
