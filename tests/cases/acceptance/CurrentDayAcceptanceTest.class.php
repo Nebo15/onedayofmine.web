da624cf9991f0925d8130c8a04c0d08693cc3051
korchasa <korchasa@gmail.com>
2012-07-04 17:40:40 +0300

    Current day test file

---
+++ tests/cases/acceptance/CurrentDayAcceptanceTest.class.php
@@ -0,0 +1,16 @@
+<?php
+lmb_require('tests/cases/odAcceptanceTestCase.class.php');
+
+class CurrentDayAcceptanceTest extends odAcceptanceTestCase
+{
+	function setUp()
+	{
+		parent::setUp();
+		odTestsTools::truncateTablesOf('Day', 'Moment', 'DayComment');
+	}
+
+	/* GET /my/current_day - запрашивается при старте приложения, возвращает текущий наполняемый день пользователя
+	POST /my/current_day/start
+	POST /my/current_day/addMoment
+	POST /my/current_day/finish */
+}
