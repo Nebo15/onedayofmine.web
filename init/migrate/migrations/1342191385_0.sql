8dd1dc3f9b8024f1af9704f326e791fd2991acec
korchasa <korchasa@gmail.com>
2012-07-14 22:26:05 +0300

    Fixed default value in migration

--- init/migrate/migrations/1342191385_0.sql
+++ init/migrate/migrations/1342191385_0.sql
@@ -1,11 +1,6 @@
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-ALTER TABLE `day` MODIFY `type` enum('working','day-off','holiday','trip','special_event') NOT NULL DEFAULT '' COMMENT '' COLLATE utf8_general_ci;
-#
-#  Fieldformat of 'day.type' changed from 'enum('working','day-off','holiday','trip','special-event') NOT NULL DEFAULT '' COMMENT '' COLLATE utf8_general_ci to enum('working','day-off','holiday','trip','special_event') NOT NULL DEFAULT '' COMMENT '' COLLATE utf8_general_ci. Possibly data modifications needed!
-#
-
-
+ALTER TABLE `day` MODIFY `type` enum('working','day-off','holiday','trip','special_event') NOT NULL COMMENT '' COLLATE utf8_general_ci;
 
 SET FOREIGN_KEY_CHECKS = 1;
