c39b8b76045c1219cc046a9b761f3f95d93d0743
Andrew Dryga <andrew@dryga.com>
2012-07-16 17:38:10 +0300

    Added Twitter oAuth lib

---
+++ settings/twitter.conf.php
@@ -0,0 +1,8 @@
+<?php
+$conf = array(
+  'twitter_consumer_key'      => '7jJVJCSS3rAkfyBc1LbOQ',
+  'twitter_consumer_secret'   => 'h2nlBrRpQh8OzKm45GNjRy3iPFnq30QZnNgVuG1bsg4',
+  'twitter_request_token_url' => 'https://api.twitter.com/oauth/request_token',
+  'twitter_authorize_url'     => 'https://api.twitter.com/oauth/authorize',
+  'twitter_access_token_url'  => 'https://api.twitter.com/oauth/access_token'
+);
