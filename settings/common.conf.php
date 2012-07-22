27680be62f9d1049bdded146c38b02b11e73eaec
korchasa <korchasa@gmail.com>
2012-07-22 11:39:50 +0300

    fb app namespace added to config

--- settings/common.conf.php
+++ settings/common.conf.php
@@ -4,5 +4,6 @@ $conf = array(
   'debug_enabled' => lmb_env_get('LIMB_APP_MODE') === 'devel',
 	'fb_cache_enabled' => true,
   'fb_app_id' => '395096410536617',
-  'fb_app_secret' => '67ffc19dd0fd52d953f92478f4663459'
+  'fb_app_secret' => '67ffc19dd0fd52d953f92478f4663459',
+  'fb_app_namespace' => 'one-day-of-mine'
 );
