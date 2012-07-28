d7929542d5db25cca51a1b74c7634427d4dcbd32
Andrew Dryga <andrew@dryga.com>
2012-07-28 20:26:38 +0300

    Fixed assertings and credentials bug

--- tests/cases/acceptance/TwitterUserAcceptanceTest.class.php
+++ tests/cases/acceptance/TwitterUserAcceptanceTest.class.php
@@ -10,13 +10,13 @@ class TwitterUserTest extends odUnitTestCase
   {
     parent::setUp();
 
-    $this->main_user->setTwitterUid($this->generator->twitter_credentials()[1]['uid']);
-    $this->main_user->setTwitterAccessToken($this->generator->twitter_credentials()[1]['access_token']);
+    $this->main_user->setTwitterUid($this->generator->twitter_credentials()[0]['uid']);
+    $this->main_user->setTwitterAccessToken($this->generator->twitter_credentials()[0]['access_token']);
     $this->main_user->setTwitterAccessTokenSecret($this->generator->twitter_credentials()[0]['access_token_secret']);
     $this->main_user->save();
 
-    $this->additional_user->setTwitterUid($this->generator->twitter_credentials()[0]['uid']);
-    $this->additional_user->setTwitterAccessToken($this->generator->twitter_credentials()[0]['access_token']);
+    $this->additional_user->setTwitterUid($this->generator->twitter_credentials()[1]['uid']);
+    $this->additional_user->setTwitterAccessToken($this->generator->twitter_credentials()[1]['access_token']);
     $this->additional_user->setTwitterAccessTokenSecret($this->generator->twitter_credentials()[1]['access_token_secret']);
     $this->additional_user->save();
   }
@@ -26,7 +26,8 @@ class TwitterUserTest extends odUnitTestCase
     $text = $this->generator->string();
     $social_profile= $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER);
     $tweet = $social_profile->tweet($text);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
       $this->assertEqual($text, $tweet->text);
     }
@@ -54,7 +55,7 @@ class TwitterUserTest extends odUnitTestCase
 
     $provider = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER);
     $tweet = $provider->shareDayBegin($day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
   }
@@ -68,11 +69,11 @@ class TwitterUserTest extends odUnitTestCase
     $day_url = $this->_copyDayPageToProxy($day);
 
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareDayBegin($day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
     $tweet = $this->additional_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareDayLike($day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
   }
@@ -92,7 +93,7 @@ class TwitterUserTest extends odUnitTestCase
     $moment->save();
     $moment_url = $this->_copyMomentPageToProxy($moment);
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareMomentAdd($moment_url, $day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
 
@@ -100,7 +101,7 @@ class TwitterUserTest extends odUnitTestCase
     $moment->save();
     $moment_url = $this->_copyMomentPageToProxy($moment);
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareMomentAdd($moment_url, $day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
   }
@@ -120,7 +121,7 @@ class TwitterUserTest extends odUnitTestCase
     $moment_url = $this->_copyMomentPageToProxy($moment);
 
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareMomentAdd($moment_url, $day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
 
@@ -136,11 +137,11 @@ class TwitterUserTest extends odUnitTestCase
     $day_url = $this->_copyDayPageToProxy($day);
 
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareDayBegin($day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareDayEnd($day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
   }
@@ -154,7 +155,7 @@ class TwitterUserTest extends odUnitTestCase
     $day_url = $this->_copyDayPageToProxy($day);
 
     $tweet = $this->main_user->getSocialProfile(odSocialServices::PROVIDER_TWITTER)->shareDay($day, $day_url);
-    if($this->assertTrue($tweet instanceof stdObject)) {
+    if($this->assertTrue(is_object($tweet))) {
       $this->assertTrue($tweet->id);
     }
   }
