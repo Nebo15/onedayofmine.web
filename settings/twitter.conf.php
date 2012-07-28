9edd0dcafd27706deda2fdf8344159dd0144e973
Andrew Dryga <andrew@dryga.com>
2012-07-28 18:59:18 +0300

    Added credentials of Twitter test users

--- settings/twitter.conf.php
+++ settings/twitter.conf.php
@@ -11,3 +11,13 @@ $conf = array(
 // User: ODMTestUser
 // Email: test@onedayofmine.com
 // Password: test@onedayofmine.com
+// oauth_token: 637083468-nBzWGwpdfgTqrg2H3DZwnSgBWwMkbNmxVrwCVepx
+// oauth_token_secret: 4jWX2ozuXHcY4yRwqjFBUfV08t7kFjfxBR1OCV7Y0
+// user_id: 637083468
+
+// User: ODMTestUser2
+// Email: test2@onedayofmine.com
+// Password: test2@onedayofmine.com
+// oauth_token: 718050210-SVERCoH3Zrxiw1KiBqjN3khC6tb6Rfwzkpx4D2kt
+// oauth_token_secret: KoZL6VY45Wfp0laFXhETkEdSKFdIY92YpRfCkzZu4
+// user_id: 718050210
