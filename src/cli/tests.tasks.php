4e6842860d2b667b3e9e0a8a8ecf55e25e37c295
Andrew Dryga <andrew@dryga.com>
2012-08-02 12:28:01 +0300

    Removed task_od_parse_lj from tests.tasks.php

--- src/cli/tests.tasks.php
+++ src/cli/tests.tasks.php
@@ -9,34 +9,3 @@ function task_od_remove_cache($argv)
 	lmbFs::rm(taskman_prop('PROJECT_DIR').'/var/facebook_cache');
 	lmbFs::rm(taskman_prop('PROJECT_DIR').'/var/twitter_cache');
 }
-
-/**
- * @desc Parse lj community
- * @param unknown_type $argv
- */
-function task_od_parse_lj($argv)
-{
-	$app_dir = taskman_prop('PROJECT_DIR');
-
-	lmb_require('ljparse/*.class.php');
-
-	define('LJ_COMMUNITY_NAME', 'odin-moy-den');
-
-	$mainPage = new MainPage(new MainPageRegexpParser());
-	$mainPage->getRemoteContent(LJ_COMMUNITY_NAME, 1);
-
-	$links_list = $mainPage->getLinksToContentPages();
-	$links_list = array($links_list[0], $links_list[1]);
-
-	$contentPageParser = new ContentPageRegexpParser();
-	$posts = array();
-	foreach($links_list as $post_id) {
-		$posts[$post_id] = new ContentPage($contentPageParser);
-		$posts[$post_id]->getRemoteContent(LJ_COMMUNITY_NAME, $post_id);
-	}
-
-	foreach($posts as $post_id => $post) {
-		var_dump($post->getData());
-	}
-
-}
