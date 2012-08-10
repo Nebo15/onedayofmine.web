3448081052bdd904f0bd64ef47a8a553ac218ad7
korchasa <korchasa@gmail.com>
2012-08-10 18:10:09 +0300

    new folders map

---
+++ settings/amazon.conf.php
@@ -0,0 +1,7 @@
+<?php
+$conf = array(
+  'key' => 'AKIAJTFA7Z5XJAR2B65Q',
+  'secret' => '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',
+  'default_cache_config' => __DIR__.'/../../var/',
+  'certificate_authority' => true
+);
