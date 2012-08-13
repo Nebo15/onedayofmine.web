5c137f594bab887418d8728137429c1638869161
Andrew Dryga <andrew@dryga.com>
2012-08-13 17:23:04 +0300

    getStaticUrl shouldn't return path to static dir, if no image specified

--- src/toolkit/odTools.class.php
+++ src/toolkit/odTools.class.php
@@ -81,8 +81,9 @@ class odTools extends lmbAbstractTools
 
   function getStaticUrl($path = '')
   {
-    if(null === $path)
+    if(!$path)
       return null;
+
     return lmb_env_get('STATIC_HOST_URL').$path;
   }
 
