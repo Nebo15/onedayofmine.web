2f1264f320c85e368e42f57796e8f2c675c366b1
Andrew Dryga <andrew@dryga.com>
2012-08-14 09:56:03 +0300

    Misstypes in acceptance test lmb_require

--- tests/cases/acceptance/SocialAcceptanceTest.class.php
+++ tests/cases/acceptance/SocialAcceptanceTest.class.php
@@ -1,5 +1,5 @@
 <?php
-lmb_require('tests/unit/odAcceptanceTestCase.class.php');
+lmb_require('tests/cases/acceptance/odAcceptanceTestCase.class.php');
 
 class SocialAcceptanceTest extends odAcceptanceTestCase
 {
