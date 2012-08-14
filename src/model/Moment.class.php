54ea797d86618d4aec9c296a695f58fb9e251a9d
Andrew Dryga <andrew@dryga.com>
2012-08-14 14:50:16 +0300

    ImageModel used in Day, Moment and User [Issue #85]

--- src/model/Moment.class.php
+++ src/model/Moment.class.php
@@ -1,4 +1,5 @@
 <?php
+lmb_require('src/model/ImageModel.class.php');
 
 /**
  * @api
@@ -10,16 +11,8 @@
  * @method string getDescription()
  * @method void setFbAccessToken(string $fb_access_token)
  */
-class Moment extends BaseModel
+class Moment extends ImageModel
 {
-  const IMAGE_ORIG = 'orig';
-  const IMAGE_SMALL = '266x266';
-  const IMAGE_BIG = '532x532';
-
-  protected $_db_table_name = 'moment';
-  protected $_default_sort_params = array('id'=>'asc');
-  protected $_lazy_attributes = array('description');
-
   protected function _defineRelations()
   {
     $this->_has_many = array (
@@ -43,8 +36,7 @@ class Moment extends BaseModel
     $moment->id = $this->getId();
     $moment->day_id = $this->getDayId();
     $moment->description = $this->getDescription();
-    $moment->image_266 = lmbToolkit::instance()->getStaticUrl($this->getImageSmall(true));
-    $moment->image_532 = lmbToolkit::instance()->getStaticUrl($this->getImageBig(true));
+    $this->showImages($moment);
     $moment->time = self::stampToIso($this->getTime(), $this->getTimezone());
     $moment->likes_count = $this->getLikesCount() ?: 0;
     $moment->ctime = $this->getCtime();
@@ -52,60 +44,13 @@ class Moment extends BaseModel
     return $moment;
   }
 
-  function attachImage($content)
+  protected function _getAdditionalPlaceholders(&$placeholders)
   {
-    $extension = lmbToolkit::instance()->getImageHelper()->getImageExtensionByImageContent($content);
-    $this->setImageExt($extension);
-
-    $orig_file = lmbToolkit::instance()->getAbsolutePath($this->getImageOrig());
-    lmbFs::safeWrite($orig_file, $content);
-
-    $small_file = lmbToolkit::instance()->getAbsolutePath($this->getImageSmall());
-    $helper = new lmbConvertImageHelper($orig_file);
-    $helper->resizeAndCropFrame(array('width' => 266, 'height' => 266));
-    $helper->save($small_file);
-
-    $small_file = lmbToolkit::instance()->getAbsolutePath($this->getImageBig());
-    $helper = new lmbConvertImageHelper($orig_file);
-    $helper->resizeAndCropFrame(array('width' => 532, 'height' => 532));
-    $helper->save($small_file);
-  }
-
-  function getImageOrig($static = false)
-  {
-    return $this->getImagePath(self::IMAGE_ORIG, $static);
-  }
-
-  function getImageSmall($static = false)
-  {
-    return $this->getImagePath(self::IMAGE_SMALL, $static);
-  }
-
-  function getImageBig($static = false)
-  {
-    return $this->getImagePath(self::IMAGE_BIG, $static);
-  }
-
-  function getImagePath($size_variation, $static = false)
-  {
-    if(!$this->getImageExt())
-      return '';
-    if(!$this->getId())
-      throw new lmbException("Can't create image path, because moment have no id");
-    if(!$this->getDayId())
-      throw new lmbException("Can't create image path, because moment have no DAY id");
     if(!$this->getDay()->getUser() || !$this->getDay()->getUser()->getId())
-      throw new lmbException("Can't create image path, because moment have no user");
-    $user_id = $this->getDay()->getUser()->getId();
-    $day_id = $this->getDayId();
-    $hash = sha1('s0l7&p3pp$r'.$user_id.$day_id.$this->getId());
-    $ext = $this->getImageExt();
+      throw new Exception("Can't create image path, because entity have no corresponding User.", array('class' => get_called_class()));
 
-    $path = "{$user_id}/days/{$day_id}/{$hash}_{$size_variation}.{$ext}";
-
-    if(!$static)
-      $path = "users/{$path}";
-
-    return $path;
+    $placeholders[':user_id'] = $this->getDay()->getUser()->getId();
+    $placeholders[':day_id']  = $this->getDay()->getId();
+    $placeholders[':hash']    = sha1('s0l7&p3pp$r'.$this->getDay()->getUser()->getId().$this->getId().$this->getDay()->getId());
   }
 }
