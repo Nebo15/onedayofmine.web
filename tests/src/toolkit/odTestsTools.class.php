1d093a2f1cf811a5b4cacbde475b5f5a47f89838
Andrew Dryga <andrew@dryga.com>
2012-08-17 14:12:04 +0300

    Spaces in odTestTools

--- tests/src/toolkit/odTestsTools.class.php
+++ tests/src/toolkit/odTestsTools.class.php
@@ -5,52 +5,52 @@ lmb_require('tests/src/odModelToEntitiesWriter.class.php');
 
 class odTestsTools extends lmbAbstractTools
 {
-	/**
-	 * @var odPostmanWriter
-	 */
-	protected $postman_writer;
+  /**
+   * @var odPostmanWriter
+   */
+  protected $postman_writer;
 
-	/**
-	 * @var odApiToMarkDownWriter
-	 */
-	protected $api_to_markdown_writer;
+  /**
+   * @var odApiToMarkDownWriter
+   */
+  protected $api_to_markdown_writer;
 
-	/**
-	 * @var odModelToEntitiesWriter
-	 */
-	protected $model_to_entities_writer;
+  /**
+   * @var odModelToEntitiesWriter
+   */
+  protected $model_to_entities_writer;
 
-	/**
-	 * @return odPostmanWriter
-	 */
-	function getPostmanWriter()
-	{
-		if(!$this->postman_writer)
-			$this->postman_writer = new odPostmanWriter(lmb_env_get('APP_DIR').'/www/api_doc/postman.json');
-		return $this->postman_writer;
-	}
+  /**
+   * @return odPostmanWriter
+   */
+  function getPostmanWriter()
+  {
+    if(!$this->postman_writer)
+      $this->postman_writer = new odPostmanWriter(lmb_env_get('APP_DIR').'/www/api_doc/postman.json');
+    return $this->postman_writer;
+  }
 
-	/**
-	 * @return odApiToMarkDownWriter
-	 */
-	function getApiToMarkdownWriter()
-	{
-		if(!$this->api_to_markdown_writer)
-			$this->api_to_markdown_writer = new odApiToMarkdownWriter(lmb_env_get('APP_DIR').'/www/api_doc/examples.markdown');
-		return $this->api_to_markdown_writer;
-	}
+  /**
+   * @return odApiToMarkDownWriter
+   */
+  function getApiToMarkdownWriter()
+  {
+    if(!$this->api_to_markdown_writer)
+      $this->api_to_markdown_writer = new odApiToMarkdownWriter(lmb_env_get('APP_DIR').'/www/api_doc/examples.markdown');
+    return $this->api_to_markdown_writer;
+  }
 
-	/**
-	 * @return odModelToEntitiesWriter
-	 */
-	function getModelToEntitiesWriter() {
-		if(!$this->model_to_entities_writer)
-			$this->model_to_entities_writer = new odModelToEntitiesWriter(lmb_env_get('APP_DIR').'/www/api_doc/entities.markdown');
-		return $this->model_to_entities_writer;
-	}
+  /**
+   * @return odModelToEntitiesWriter
+   */
+  function getModelToEntitiesWriter() {
+    if(!$this->model_to_entities_writer)
+      $this->model_to_entities_writer = new odModelToEntitiesWriter(lmb_env_get('APP_DIR').'/www/api_doc/entities.markdown');
+    return $this->model_to_entities_writer;
+  }
 
-	function getTestsUsers($all = false)
-	{
+  function getTestsUsers($all = false)
+  {
     $fb_test_users = lmbToolkit::instance()->loadTestsUsersInfo();
     if($all)
       $users_infos = $fb_test_users;
@@ -64,45 +64,45 @@ class odTestsTools extends lmbAbstractTools
     }
 
     $users = array();
-		foreach($users_infos as $user_info)
-		{
-			$user_info = (object) $user_info;
-			$user = new User();
-			$user->setFbUid($user_info->id);
-			$user->setFbAccessToken($user_info->access_token);
-			$user->import((new FacebookProfile($user))->getInfo());
+    foreach($users_infos as $user_info)
+    {
+      $user_info = (object) $user_info;
+      $user = new User();
+      $user->setFbUid($user_info->id);
+      $user->setFbAccessToken($user_info->access_token);
+      $user->import((new FacebookProfile($user))->getInfo());
 
       $settings = $user->getSettings();
       $settings->setSocialShareFacebook(0);
       $settings->setSocialShareTwitter(0);
       $user->setSettings($settings);
 
-			$users[] = $user;
-		}
-		return $users;
-	}
+      $users[] = $user;
+    }
+    return $users;
+  }
 
-	static function truncateTablesOf($model_classes)
-	{
-		if(!is_array($model_classes))
-			$model_classes = func_get_args();
-		foreach($model_classes as $model_class)
-		{
-			$model_class::delete();
-		}
-	}
+  static function truncateTablesOf($model_classes)
+  {
+    if(!is_array($model_classes))
+      $model_classes = func_get_args();
+    foreach($model_classes as $model_class)
+    {
+      $model_class::delete();
+    }
+  }
 
-	function checkServer($uri_string)
-	{
-		$uri = new lmbUri($uri_string);
-		$fp = @fsockopen($uri->getHost(), $uri->getPort() ?: 80, $errno, $errstr, 30);
-		if (!$fp) {
-			echo("Can't connect to server '$uri_string': $errstr ($errno)\n");
-			exit(1);
-		} else {
-			fclose($fp);
-		}
-	}
+  function checkServer($uri_string)
+  {
+    $uri = new lmbUri($uri_string);
+    $fp = @fsockopen($uri->getHost(), $uri->getPort() ?: 80, $errno, $errstr, 30);
+    if (!$fp) {
+      echo("Can't connect to server '$uri_string': $errstr ($errno)\n");
+      exit(1);
+    } else {
+      fclose($fp);
+    }
+  }
 }
 
 
