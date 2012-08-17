b21373cd488e3dc3b86f43ce782c88e155aecdbc
Andrew Dryga <andrew@dryga.com>
2012-08-17 11:34:44 +0300

    Remove constructor and improve validators in odPostingService [Issue #98]

--- src/service/odPostingService.class.php
+++ src/service/odPostingService.class.php
@@ -3,33 +3,46 @@ lmb_require('src/service/social_profile/SharesInterface.class.php');
 
 class odPostingService implements SharesInterface
 {
-  protected $twitter_profile;
-  protected $facebook_profile;
-
-  public function __construct()
-  {
-    $user                   = lmbToolkit::instance()->getUser();
-    $this->facebook_profile = new FacebookProfile($user);
-    $this->twitter_profile  = new TwitterProfile($user);
-  }
-
   /**
    * @throws lmbException
    */
   protected function share($name, $args)
   {
-    $user     = lmbToolkit::instance()->getUser();
+    static $facebook_profile;
+    static $twitter_profile;
+
+    $user = lmbToolkit::instance()->getUser();
+    lmb_assert_true($user, 'User is not set!');
     $settings = $user->getSettings();
 
-    if($settings->getSocialShareFacebook() && $user->getFbAccessToken()) {
-      if(!call_user_func_array(array($this->facebook_profile, $name), $args))
-        throw new lmbException("Can't share with Facebook.", array('function_name' => $name, 'function_args' => $args));
-    }
+    // Facebook
+    if($settings->getSocialShareFacebook())
+      if($user->getFbAccessToken()) {
+        $facebook_profile = $facebook_profile ?: new FacebookProfile($user);
 
-    if($settings->getSocialShareTwitter() && $user->getTwitterAccessToken() && $user->getTwitterAccessTokenSecret()) {
-      if(!call_user_func_array(array($this->twitter_profile, $name), $args))
-        throw new lmbException("Can't share with Twitter.", array('function_name' => $name, 'function_args' => $args));
-    }
+        if(!call_user_func_array(array($facebook_profile, $name), $args))
+          throw new lmbException("Can't share with Facebook.", array('function_name' => $name, 'function_args' => $args));
+      }
+      else
+        lmbToolkit::instance()->log()->warning('Facebook sharing is on, while facebook token is empty.', array(
+          'user_id'             => $user->getId(),
+          'access_token'        => $user->getFbAccessToken(),
+        ));
+
+    // Twitter
+    if($settings->getSocialShareTwitter())
+      if($user->getTwitterAccessToken() && $user->getTwitterAccessTokenSecret()) {
+        $twitter_profile = $twitter_profile ?: new TwitterProfile($user);
+
+        if(!call_user_func_array(array($twitter_profile, $name), $args))
+          throw new lmbException("Can't share with Twitter.", array('function_name' => $name, 'function_args' => $args));
+      }
+      else
+        lmbToolkit::instance()->log()->warning('Twitter sharing is on, while twitter is not connected.', array(
+          'user_id'             => $user->getId(),
+          'access_token'        => $user->getTwitterAccessToken(),
+          'access_token_secret' => $user->getTwitterAccessTokenSecret(),
+        ));
   }
 
   public function shareDayBegin(Day $day)
