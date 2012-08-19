2eea83c3e724fbe642135be185be5b14a41403f4
korchasa <korchasa@gmail.com>
2012-08-19 20:31:37 +0300

    Fix for invalid exif data

--- src/service/ImageHelper.class.php
+++ src/service/ImageHelper.class.php
@@ -14,8 +14,8 @@ class ImageHelper {
   {
     $cords = $this->exifGPSToCords($exif);
     return array(
-      'latitude'  => $cords['latitude']['degrees']+((($cords['latitude']['minutes']*60)+($cords['latitude']['seconds']))/3600),
-      'longitude' => $cords['longitude']['degrees']+((($cords['longitude']['minutes']*60)+($cords['longitude']['seconds']))/3600)
+      'latitude'  => $cords['latitude'] ? $cords['latitude']['degrees']+((($cords['latitude']['minutes']*60)+($cords['latitude']['seconds']))/3600) : 0,
+      'longitude' => $cords['longitude'] ? $cords['longitude']['degrees']+((($cords['longitude']['minutes']*60)+($cords['longitude']['seconds']))/3600): 0
     );
   }
 
