8000bf63a0793e93c39961f2f8805ff0308a1dca
Andrew Dryga <andrew@dryga.com>
2012-08-27 09:43:32 +0300

    Remove unnecessary data from output of finish comment

--- src/controller/BaseJsonController.class.php
+++ src/controller/BaseJsonController.class.php
@@ -264,6 +264,8 @@ abstract class BaseJsonController extends lmbController
   protected function _attachFinishComment(stdClass $export, Day $day)
   {
     $export->finish_comment = $day->getFinishComment() ? $day->getFinishComment()->exportForApi() : null;
+    unset($export->finish_comment->user_id);
+    unset($export->finish_comment->day_id);
   }
 
   protected function _attachDayMoments(stdClass $day_export, $day)
