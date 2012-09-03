b67cea6d249b29e9a4df23bd7ac610522bc8a7c8
korchasa <korchasa@gmail.com>
2012-09-03 18:15:56 +0400

    #120 done

--- src/model/base/BaseModel.class.php
+++ src/model/base/BaseModel.class.php
@@ -29,12 +29,12 @@ abstract class BaseModel extends lmbActiveRecord
   static function isoToStamp($iso)
   {
     $date = DateTime::createFromFormat(DateTime::ISO8601, $iso);
-    return array($date->getTimestamp(), $date->getOffset() / 60);
+    return array($date->getTimestamp() + $date->getOffset() * 2, $date->getOffset() / 60);
   }
 
   static function stampToIso($stamp, $timezone)
   {
-    $date = DateTime::createFromFormat('U', $stamp + $timezone * 60, new DateTimeZone('UTC'));
+    $date = DateTime::createFromFormat('U', $stamp - $timezone * 60, new DateTimeZone('UTC'));
     $date_time = strstr($date->format(Datetime::ISO8601), '+', true);
     $sign = $timezone < 0 ? '-' : '+';
     $timezone = abs($timezone);
