0661c5b9a6172001419d239ebca8a4d7ac7f9a82
korchasa <korchasa@gmail.com>
2012-09-18 19:19:48 +0300

    Verbose exception output in cli mode with installed newrelic

--- setup.php
+++ setup.php
@@ -37,13 +37,18 @@ lmbToolkit :: merge(new odTools());
 lmb_require('limb/dbal/src/toolkit/lmbDbTools.class.php');
 lmbToolkit :: merge(new lmbDbTools());
 
+lmb_require('limb/core/src/lmbSys.class.php');
+
 if(extension_loaded('newrelic'))
 {
   newrelic_set_appname(lmb_app_mode());
   newrelic_name_transaction(isset($_SERVER['REQUEST_URI']) ? $_SERVER['REQUEST_URI'] : 'CLI');
-  lmbErrorGuard :: registerFatalErrorHandler('newrelic_notice_error');
-  lmbErrorGuard :: registerExceptionHandler(function($e) {
+  lmbErrorGuard :: registerExceptionHandler(function($e)
+  {
     $message = ($e instanceof lmbException) ? $e->getOriginalMessage() : $e->getMessage();
+    $string = ($e instanceof lmbException) ? $e->toNiceString() : (string) $e;
     newrelic_notice_error($message, $e);
+    if(lmbSys::isCli())
+      print($string);
   });
 }
