05422007e0d8167918a544400df4119e78ea2333
korchasa <korchasa@gmail.com>
2012-09-26 18:29:18 +0300

    Removed reflection uses

--- src/odApplication.class.php
+++ src/odApplication.class.php
@@ -1,28 +1,35 @@
 <?php
 lmb_require('limb/web_app/src/lmbWebApplication.class.php');
+lmb_require('src/filter/ErrorHandlingFilter.class.php');
+lmb_require('src/filter/InputOutputLogWritterFilter.class.php');
+lmb_require('limb/dbal/src/filter/lmbAutoDbTransactionFilter.class.php');
+lmb_require('limb/profile/src/filter/lmbProfileReportingFilter.class.php');
+lmb_require('src/filter/SessionStartupFilter.class.php');
+lmb_require('src/filter/RequestDispatchingFilter.class.php');
+lmb_require('limb/web_app/src/request/lmbRoutesRequestDispatcher.class.php');
+lmb_require('limb/web_app/src/filter/lmbResponseTransactionFilter.class.php');
+lmb_require('limb/web_app/src/filter/lmbActionPerformingFilter.class.php');
+lmb_require('limb/web_app/src/filter/lmbViewRenderingFilter.class.php');
 
 class odApplication extends lmbWebApplication
 {
   protected function _registerFilters()
   {
-    $this->registerFilter(new lmbHandle('src/filter/ErrorHandlingFilter'));
+    $this->registerFilter(new ErrorHandlingFilter());
 
-    $this->registerFilter(new lmbHandle('src/filter/InputOutputLogWritterFilter'));
+    $this->registerFilter(new InputOutputLogWritterFilter());
 
-    $this->registerFilter(new lmbHandle('limb/dbal/src/filter/lmbAutoDbTransactionFilter'));
-    $this->registerFilter(new lmbHandle('limb/profile/src/filter/lmbProfileReportingFilter'));
+    $this->registerFilter(new lmbAutoDbTransactionFilter());
+    $this->registerFilter(new lmbProfileReportingFilter());
 
-    $this->registerFilter(new lmbHandle('src/filter/SessionStartupFilter'));
+    $this->registerFilter(new SessionStartupFilter());
 
-    $this->registerFilter(
-      new lmbHandle('src/filter/RequestDispatchingFilter',
-        array(new lmbHandle('limb/web_app/src/request/lmbRoutesRequestDispatcher')))
-    );
-    $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbResponseTransactionFilter'));
+    $this->registerFilter(new RequestDispatchingFilter(new lmbRoutesRequestDispatcher()));
+    $this->registerFilter(new lmbResponseTransactionFilter());
 
-    $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbActionPerformingFilter'));
+    $this->registerFilter(new lmbActionPerformingFilter());
 
-    $this->registerFilter(new lmbHandle('limb/web_app/src/filter/lmbViewRenderingFilter'));
+    $this->registerFilter(new lmbViewRenderingFilter());
   }
 }
 
