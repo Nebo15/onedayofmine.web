a00a98e4539ed6b3c385ac1c1bc0fe097b951978
Andrew Dryga <andrew@dryga.com>
2012-09-28 19:55:53 +0300

    Rewrited odIntegrationTestCase

--- tests/cases/integration/odIntegrationTestCase.class.php
+++ tests/cases/integration/odIntegrationTestCase.class.php
@@ -13,14 +13,6 @@ abstract class odIntegrationTestCase extends WebTestCase
    */
   protected $generator;
   /**
-   * @var lmbSimpleDb
-   */
-  protected $db;
-  /**
-   * @var [type]
-   */
-  protected $last_profile_info;
-  /**
    * @var User
    */
   protected $main_user;
@@ -38,122 +30,39 @@ abstract class odIntegrationTestCase extends WebTestCase
   {
     $this->generator = new odObjectMother();
     $this->toolkit = lmbToolkit::instance();
+
     $this->toolkit->setConfIncludePath('tests/cases/integration/settings;tests/settings;settings');
     $this->toolkit->resetConfs();
     $this->toolkit->resetFileLocators();
+
     parent::setUp();
+
     $this->toolkit->truncateDb();
+
     list($this->main_user, $this->additional_user) = $this->toolkit->getTestsUsers($quiet = false);
   }
 
-  function get($url, $params = array())
+  protected function assertResponseClass(stdClass $response, $message = "Wrong response structure")
   {
-    $raw_response = parent::get(lmbToolkit::instance()->getSiteUrl($url), $params);
-    $result = $this->_decodeResponse($raw_response);
-    if(
-      !property_exists($result, 'result') ||
-      !property_exists($result, 'errors') ||
-      !property_exists($result, 'status') ||
-      !property_exists($result, 'code')
-    )
-
-    $this->fail('Wrong response structure:'.PHP_EOL.$raw_response);
-    return $result;
+    $this->assertPropertys($response, [
+      'result',
+      'errors',
+      'status',
+      'code',
+    ], $message);
   }
 
-  function post($url, $params = array(), $content_type = false)
+  function assertResponse($code, $message = 'Expecting response in [%s] got [%s] in response: %s')
   {
-    if(is_object($params))
-      $params = (array) $params;
-    $raw_response = parent::post(lmbToolkit::instance()->getSiteUrl($url), $params);
-    $result = $this->_decodeResponse($raw_response);
-    if(
-      !property_exists($result, 'result') ||
-      !property_exists($result, 'errors') ||
-      !property_exists($result, 'status') ||
-      !property_exists($result, 'code')
-    )
+    $response_code = $this->getBrowser()->getResponseCode();
 
-    $this->fail('Wrong response structure:'.PHP_EOL.$raw_response);
-    return $result;
-  }
-
-  protected function _addRecordsToWriters($url_path, $params, $method, $response)
-  {
-    $trace = debug_backtrace();
-    $class_name = get_called_class();
-    $method_name = $trace[2]['function'];
-    $class_ref = new ReflectionClass($class_name);
-    $method_ref = $class_ref->getMethod($method_name);
-    $call_name = str_replace('AcceptanceTest', '', $class_name).' - '.str_replace('test', '', $method_name);
-
-    $doc_comment = DocCommentParser::tokenize($method_ref->getDocComment());
-
-    if(!$doc_comment->hasGroup('api'))
-      return;
-
-    lmbToolkit::instance()
-      ->getPostmanWriter()
-      ->addRequest($call_name, $url_path, $method, $params);
-
-    lmbToolkit::instance()
-      ->getApiToMarkdownWriter()
-      ->addRequest($call_name, $method, $url_path, $params, $response, $doc_comment);
-
-  }
-
-  protected function _loginAndSetCookie(User $user)
-  {
-    $res = $this->_login($user);
-    $this->setCookie('token', $user->getFacebookAccessToken());
-    return $res;
-  }
-
-  protected function _login(User $user)
-  {
-    $params = array(
-      'token' => $user->getFacebookAccessToken(),
-      'device_token' => $this->generator->string(64)
+    return $this->assertEqual(
+      $code,
+      $response_code,
+      sprintf($message,
+        $code, $response_code, $this->getBrowser()->getContent()
+      )
     );
-    $res = $this->post('auth/login/', $params);
-    $this->assertResponse(200);
-    $this->assertProperty($res->result, 'name');
-    return $res;
-  }
-
-  protected function _decodeResponse($raw_response)
-  {
-    $decoded_body = json_decode($raw_response);
-    if ($decoded_body === null) {
-      throw new lmbException("Can't parse response", array(
-          'url' => $this->getUrl(),
-          'raw' => $raw_response
-      ));
-    }
-
-    return $decoded_body;
-  }
-
-  protected function _splitBodyAndProfile($raw_response)
-  {
-    $profile_info_begin = strpos($raw_response, "}{\"main\"");
-
-    if (false !== $profile_info_begin) {
-      $body = substr($raw_response, 0, $profile_info_begin + 1);
-      $profile = substr($raw_response, $profile_info_begin + 1);
-      return array($body, $profile);
-    } else {
-      return array($raw_response, '{}');
-    }
-  }
-
-  function assertResponse($responses, $message = '%s')
-  {
-    $responses = (is_array($responses) ? $responses : array($responses));
-    $code = $this->getBrowser()->getResponseCode();
-    $message = sprintf('%s', "Expecting response in [" .
-      implode(", ", $responses) . "] got [$code] in response:".($this->getBrowser()->getContent()));
-    return $this->assertTrue(in_array($code, $responses), $message);
   }
 
   protected function assertImageUrl($url, $message = "Invalid image url '%s'")
@@ -185,5 +94,62 @@ abstract class odIntegrationTestCase extends WebTestCase
     } catch (HttpException $ex) {
       $this->fail($ex->getMessage());
     }
+  }
+
+  function get($url, $params = array())
+  {
+    if(is_object($params))
+      $params = (array) $params;
+
+    $raw_response = parent::get(lmbToolkit::instance()->getSiteUrl($url), $params);
+    $response = $this->_decodeResponse($raw_response);
+
+    $this->assertResponseClass($response, "Wrong response structure: {$raw_response}.");
+
+    return $response;
+  }
+
+  function post($url, $params = array(), $content_type = false)
+  {
+    if(is_object($params))
+      $params = (array) $params;
+
+    $raw_response = parent::post(lmbToolkit::instance()->getSiteUrl($url), $params);
+    $response = $this->_decodeResponse($raw_response);
+
+    $this->assertResponseClass($response, "Wrong response structure: {$raw_response}.");
+
+    return $response;
+  }
+
+  protected function _loginAndSetCookie(User $user)
+  {
+    return $this->_login($user);
+  }
+
+  protected function _login(User $user)
+  {
+    $response = $this->post('auth/login/', [
+      'token'        => $user->getFacebookAccessToken(),
+      'device_token' => $this->generator->string(64)
+    ]);
+
+    if($this->assertResponse(200))
+      $this->assertProperty($response->result, 'name');
+
+    return $response;
+  }
+
+  protected function _decodeResponse($raw_response)
+  {
+    $decoded_body = json_decode($raw_response);
+    if ($decoded_body === null) {
+      throw new lmbException("Can't parse response", array(
+          'url' => $this->getUrl(),
+          'raw' => $raw_response
+      ));
+    }
+
+    return $decoded_body;
   }
 }
