c142274f4bc16f67bf915ebb99d58d6181d65b17
korchasa <korchasa@gmail.com>
2012-09-28 15:23:36 +0300

    default cache dsn

--- settings/cache.conf.php
+++ settings/cache.conf.php
@@ -2,5 +2,6 @@
 
 $conf = array();
 $conf['cache_enabled'] = true;
+$conf['default_cache_dsn'] = "file:///".lmb_var_dir()."/sessions";
 $conf['session_cache_dsn'] = "file:///".lmb_var_dir()."/sessions";
 $conf['db_info_cache_dsn'] = "file:///".lmb_var_dir()."/db_info";
