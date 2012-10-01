0eab71a2bfbeb329f87cac91be8f9edadc5ca212
korchasa <korchasa@gmail.com>
2012-10-01 18:44:49 +0300

    Noop operation

--- src/controller/MainPageController.class.php
+++ src/controller/MainPageController.class.php
@@ -3,12 +3,17 @@ lmb_require('src/controller/BaseJsonController.class.php');
 lmb_require('tests/unit/odTestsTools.class.php');
 lmb_require('src/Json.class.php');
 
-class MainPageController extends lmbController
+class MainPageController extends BaseJsonController
 {
   function doDeploy()
   {
     echo '<pre>';
     system(lmb_env_get('APP_DIR').'/cli/update.sh');
     die();
+  }
+
+  function doGuestNoop()
+  {
+    return $this->_answerOk();
   }
 }
