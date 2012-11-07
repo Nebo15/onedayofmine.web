9d0b320c4da514ededf23e63cb42691e5dfb2866
Andrew Dryga <andrew@dryga.com>
2012-11-07 18:49:29 +0200

    Sphinx install script

--- settings/third-party/sphinx/install.sh
+++ settings/third-party/sphinx/install.sh
@@ -4,6 +4,8 @@ wget http://sphinxsearch.com/files/sphinx-2.0.6-release.tar.gz
 tar -xzf sphinx-2.0.6-release.tar.gz
 cd sphinx-2.0.6-release
 cd ./api/libsphinxclient/
+touch buildconf.sh
+chmod +x buildconf.sh
 ./buildconf.sh
 ./configure
 checkinstall
