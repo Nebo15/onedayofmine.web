4e09e4e132c3eb9d3b4201fa06ef1665dd0cacba
Andrew Dryga <andrew@dryga.com>
2012-11-08 01:15:58 +0200

    Modified sphinx install script

--- settings/third-party/sphinx/install.sh
+++ settings/third-party/sphinx/install.sh
@@ -1,4 +1,15 @@
+PROJECT_DIR=$(realpath $(dirname $0)/../../../)
+
 apt-get install sphinxsearch
+mkdir -p /var/log/sphinx/searchd
+mkdir -p /var/data/sphinx
+indexer --config $PROJECT_DIR/settings/third-party/sphinx/sphinx.conf days
+indexer --config $PROJECT_DIR/settings/third-party/sphinx/sphinx.conf users
+indexer --config $PROJECT_DIR/settings/third-party/sphinx/sphinx.conf days_delta
+indexer --config $PROJECT_DIR/settings/third-party/sphinx/sphinx.conf users_delta
+chown -R sphinxsearch:sphinxsearch /var/log/sphinx/searchd
+chown -R sphinxsearch:sphinxsearch /var/data/sphinx
+echo "START=yes" > /etc/default/sphinxsearch
 cd /tmp
 wget http://sphinxsearch.com/files/sphinx-2.0.6-release.tar.gz
 tar -xzf sphinx-2.0.6-release.tar.gz
