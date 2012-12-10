7d2d262d9b4516fe14b285074afb6641600a3cf5
korchasa <korchasa@gmail.com>
2012-12-10 12:44:54 +0200

    verbose sphinx script

--- cli/crontab_gen.php
+++ cli/crontab_gen.php
@@ -9,10 +9,10 @@ echo "* *	* * *	www-data cd $dir; ./limb od_job_worker $log_str";
 # Sphinx
 ## Users
 $sphinx_config = "--config $dir/settings/third-party/sphinx/sphinx.conf";
-echo "* * * * * sphinxsearch indexer $sphinx_config --rotate --quiet users_delta $log_str";
-echo "* * * * * sphinxsearch indexer $sphinx_config --rotate --quiet --merge users users_delta $log_str";
-echo "1 4 * * * sphinxsearch indexer $sphinx_config--rotate --quiet users $log_str";
+echo "* * * * * sphinxsearch indexer $sphinx_config --rotate users_delta $log_str";
+echo "* * * * * sphinxsearch indexer $sphinx_config --rotate --merge users users_delta $log_str";
+echo "1 4 * * * sphinxsearch indexer $sphinx_config--rotate -users $log_str";
 ## Days
-echo "* * * * * sphinxsearch indexer $sphinx_config --rotate --quiet days_delta $log_str";
-echo "* * * * * sphinxsearch indexer $sphinx_config --rotate --quiet --merge days days_delta $log_str";
-echo "1 4	* * *	sphinxsearch indexer $sphinx_config --rotate --quiet days $log_str";
+echo "* * * * * sphinxsearch indexer $sphinx_config --rotate days_delta $log_str";
+echo "* * * * * sphinxsearch indexer $sphinx_config --rotate --merge days days_delta $log_str";
+echo "1 4	* * *	sphinxsearch indexer $sphinx_config --rotate days $log_str";
