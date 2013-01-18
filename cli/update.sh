d1c646350c522d293616fb3512660a47e41e5595
Stanislav Korchagin <korchasa@gmail.com>
2013-01-18 17:47:46 +0200

    keep logs on update

--- cli/update.sh
+++ cli/update.sh
@@ -3,7 +3,11 @@
 PROJECT_DIR=$(realpath $(dirname $0)/../)
 cd $PROJECT_DIR
 git pull origin develop
-rm -rf ./var/*
+rm -rf ./var/compiled
+rm -rf ./var/db_info
+rm -rf ./var/default
+rm -rf ./var/fb_cache
+rm -rf ./var/sessions
 mkdir ./var/logs
 ./lib/limb/limb migrate_run
 ./lib/limb/limb od_calc_interest
