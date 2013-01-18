8177b1ab50bd5a0af37383e452bab305e895fe9d
Stanislav Korchagin <korchasa@gmail.com>
2013-01-18 16:09:48 +0200

    fb cache settings

--- settings/cache.conf.php
+++ settings/cache.conf.php
@@ -3,5 +3,6 @@
 $conf = array();
 $conf['cache_enabled'] = true;
 $conf['default_cache_dsn'] = "file:///".lmb_var_dir()."/default";
+$conf['fb_cache_dsn'] = "file:///".lmb_var_dir()."/fb";
 $conf['session_cache_dsn'] = "file:///".lmb_var_dir()."/sessions";
 $conf['db_info_cache_dsn'] = "file:///".lmb_var_dir()."/db_info";
