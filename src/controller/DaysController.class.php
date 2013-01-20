61d010c3f6c4ae597da8392233a98e9c80cb9223
Stanislav Korchagin <korchasa@gmail.com>
2013-01-20 20:27:13 +0200

    get image content by url (amazon s3 support)

--- src/controller/DaysController.class.php
+++ src/controller/DaysController.class.php
@@ -65,7 +65,8 @@ class DaysController extends BaseJsonController
 
 	  if($cover_moment_id = $this->request->get('cover_moment_id')) {
 		  $cover_moment = Moment::findById($cover_moment_id);
-		  $day->attachImage(file_get_contents($cover_moment->getImagePath(['width' => 532])));
+		  $url = $this->toolkit->getSiteUrl($cover_moment->getImage(['width' => 532]));
+		  $day->attachImage(file_get_contents($url));
 		  $day->save();
 	  }
 
