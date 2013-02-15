d37956a8085575405263cb94a531610b744a9352
Stanislav Korchagin <korchasa@gmail.com>
2013-02-15 06:38:00 +0200

    news merging

--- src/controller/PagesController.class.php
+++ src/controller/PagesController.class.php
@@ -26,7 +26,7 @@ class PagesController extends lmbController
 			$user  = $this->toolkit->getUser();
 			$news = $user->getNews();
 
-			$this->news = $this->_toFlatArray($this->toolkit->getExportHelper()->exportNewsItems($news));
+			$this->news = $this->_mergeNews($news);
 
 			return $this->doImport();
 		}
@@ -34,6 +34,37 @@ class PagesController extends lmbController
 		{
 			$this->setTemplate('pages/display_guest.phtml');
 		}
+	}
+
+	protected function _mergeNews($news)
+	{
+		$news = $this->toolkit->getExportHelper()->exportNewsItems($news);
+
+		$result = [];
+		$current = null;
+		foreach($news as $news_item)
+		{
+			if($current && $current->text == $news_item->text)
+			{
+				if(property_exists($news_item, 'day') && $current->day != $news_item->day)
+					$current->days[] = $news_item->day;
+				if(property_exists($news_item, 'moment') && $current->moment != $news_item->moment)
+					$current->moments[] = $news_item->moment;
+				if(property_exists($news_item, 'day_comment') && $current->day_comment != $news_item->day_comment)
+					$current->day_comments[] = $news_item->day_comment;
+				if(property_exists($news_item, 'moment_comment') && $current->moment_comment != $news_item->moment_comment)
+					$current->moments_comments[] = $news_item->moment_comment;
+			}
+			else
+			{
+				if($current)
+					$result[] = $current;
+				$current = $news_item;
+				$current->days = $current->moments = $current->day_comments = $current->moment_comments = [];
+			}
+		}
+
+		return $this->_toFlatArray($result);
 	}
 
 	function doDaysDiscover()
