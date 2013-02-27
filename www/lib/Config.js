8cbf1aa0cd68af8dc7076fa3b3cf9220a24034bc
Andrew Dryga <andrew@dryga.com>
2013-02-28 00:50:11 +0200

    oauth renamed to instagram_oauth

--- www/lib/Config.js
+++ www/lib/Config.js
@@ -2,7 +2,7 @@ var Config = {
   instagram: {
     host: 'api.instagram.com',
     client_id: '8d8b1d3774bd4767ba54b6f48b647b9a',
-    redirect_url: 'http://stage.onedayofmine.com/pages/oauth'
+    redirect_url: 'http://stage.onedayofmine.com/pages/instagram_oauth'
   },
   host: 'stage.onedayofmine.com'
 };
