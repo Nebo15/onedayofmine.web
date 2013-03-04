9304f860fca0b3cdc4a83e65ba18e6774f508ac3
Andrew Dryga <andrew@dryga.com>
2013-03-04 04:00:13 +0200

    Test simplified login

--- www/lib/Auth.js
+++ www/lib/Auth.js
@@ -1,11 +1,5 @@
 var Auth = {
   login: function(onLoginSuccess) {
-    FB.getLoginStatus(function(cached_response) {
-      if (cached_response.status === 'connected') {
-        var token = cached_response.authResponse.accessToken;
-        console.log('Token recieved: ' + token);
-        return onLoginSuccess(token);
-      } else {
         FB.login(function(response) {
           if(response.authResponse) {
             var token = response.authResponse.accessToken;
@@ -16,7 +10,6 @@ var Auth = {
           }
         });
       }
-    });
   },
 
   logout: function(callback) {
