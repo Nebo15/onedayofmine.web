e9bdc7fba495ce139d25100a2fdb776300ca5606
Andrew Dryga <andrew@dryga.com>
2013-03-04 04:02:12 +0200

    Rollback "Test simplified login"

--- www/lib/Auth.js
+++ www/lib/Auth.js
@@ -1,14 +1,22 @@
 var Auth = {
   login: function(onLoginSuccess) {
+    FB.getLoginStatus(function(cached_response) {
+      if (cached_response.status === 'connected') {
+        var token = cached_response.authResponse.accessToken;
+        console.log('FB token already set: ' + token);
+        return onLoginSuccess(token);
+      } else {
         FB.login(function(response) {
           if(response.authResponse) {
             var token = response.authResponse.accessToken;
-            console.log('Token recieved: ' + token);
+            console.log('FB token recieved: ' + token);
             return onLoginSuccess(token);
           } else {
             alert('You need to authorize application to continue using it');
           }
         });
+      }
+    });
   },
 
   logout: function(callback) {
