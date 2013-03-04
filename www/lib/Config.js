3e6aa9a04141314d867b53b8d615c039f32219e8
Andrew Dryga <andrew@dryga.com>
2013-03-04 05:12:56 +0200

    Removed Instagram from JS config

--- www/lib/Config.js
+++ www/lib/Config.js
@@ -1,8 +1,3 @@
 var Config = {
-  instagram: {
-    host: 'api.instagram.com',
-    client_id: '8d8b1d3774bd4767ba54b6f48b647b9a',
-    redirect_url: 'http://stage.onedayofmine.com/pages/oauth_instagram'
-  },
   host: 'stage.onedayofmine.com'
 };
