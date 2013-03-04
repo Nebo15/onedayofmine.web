c2929c31a1a4bbdd01f7667c8f2c5e1cdfd292b7
Andrew Dryga <andrew@dryga.com>
2013-03-04 02:15:08 +0200

    New auth flow (need to remove login_url's from API Docs)

--- src/controller/FlickrController.class.php
+++ src/controller/FlickrController.class.php
@@ -5,21 +5,34 @@ lmb_require('src/service/social_photo_source/FlickrPhotoSource.class.php');
 
 class FlickrController extends BaseJsonController
 {
-	function doLoginUrl()
-	{
-		$service = new FlickrPhotoSource($this->_getUser());
-		if($this->_getUser()->flickr_uid)
-			return $this->_answerOk();
-		else
-			return $this->_answerOk($service->getLoginUrl($this->toolkit->getSiteUrl()));
-	}
+  function doLogin()
+  {
+    $service = new FlickrPhotoSource($this->_getUser());
+    $redirect_url = '';
 
-	function doLogin()
-	{
-		$service = new FlickrPhotoSource($this->_getUser());
-		$updated_user = $service->login($this->request->get('frob'), $this->toolkit->getSiteUrl());
-		return $this->_answerOk($this->toolkit->getExportHelper()->exportUser($updated_user));
-	}
+    if($this->request->has('code')) {
+      $service->login($this->request->get('frob'), $this->toolkit->getSiteUrl());
+
+      if($this->toolkit->getSessionStorage()->get('oauth_redirect_url')) {
+        $redirect_url = $this->toolkit->getSessionStorage()->get('oauth_redirect_url');
+        $this->toolkit->getSessionStorage()->delete('oauth_redirect_url');
+      } elseif(is_array($_SERVER) && array_key_exists('HTTP_REFERER', $_SERVER)) {
+        $referer = new lmbUri($_SERVER['HTTP_REFERER']);
+        $redirect_url = $referer->toString($parts = array('path', 'query', 'anchor'));
+      }
+    } else {
+      if($this->request->has('oauth_redirect_url')) {
+        $url = new lmbUri($this->request->get('oauth_redirect_url'));
+        $this->toolkit->getSessionStorage()->set('oauth_redirect_url', $url->toString($parts = array('path', 'query', 'anchor')));
+      }
+      $redirect_url = $service->getLoginUrl($this->toolkit->getSiteUrl('instagram/login/'));
+    }
+
+    if($redirect_url)
+      $this->redirect($redirect_url);
+
+    return $this->_answerOk();
+  }
 
 	function doUser()
 	{
