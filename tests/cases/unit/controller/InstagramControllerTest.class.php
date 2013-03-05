7cc16718da50150334ff322646bd35d6f47e7798
Stanislav Korchagin <korchasa@gmail.com>
2013-03-05 18:48:08 +0200

    removed uselles test

--- tests/cases/unit/controller/InstagramControllerTest.class.php
+++ tests/cases/unit/controller/InstagramControllerTest.class.php
@@ -8,16 +8,5 @@ class InstagramControllerTest extends odControllerTestCase
 
 	function testLoginUrl()
 	{
-		$this->main_user->save();
-
-		$response = $this->get('login_url');
-
-		if($this->assertResponse(401))
-		{
-			$this->assertTrue(is_null($response->result));
-
-			$this->assertEqual(1, count($response->errors));
-			$this->assertEqual("Current user don't have permission to perform this action", $response->errors[0]);
-		}
 	}
 }
