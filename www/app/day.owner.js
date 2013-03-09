c200193189f6bd4191ca5a5cd64bc7e51789bf42
Andrew Dryga <andrew@dryga.com>
2013-03-09 02:40:37 +0200

    Try 1

--- www/app/day.owner.js
+++ www/app/day.owner.js
@@ -613,15 +613,11 @@ $(function() {
         modal_thumbnails_paginate_next_button.addClass('disabled').animate({opacity:0}, animations_speed);
         modal_thumbnails_paginate_prev_button.addClass('disabled').animate({opacity:0}, animations_speed).slideUp(animations_speed);
 
-        // next_callback(); // Loads all data, sunce it cant find minTimestamp
-        console.log('ld nxt');
-        var nxt = next_callback();
-        console.log(nxt);
-
-        nxt.success(function(tmp) {
-          console.log(tmp);
-          modal_container.modal('hide');
-          alert("Its seems that you dont have puctures in " + flickr);
+        next_callback().success(function(response) {
+          if(response.data.result.length === 0) {
+            modal_container.modal('hide');
+            alert("Its seems that you dont have puctures in " + flickr);
+          }
         });
 
         return;
