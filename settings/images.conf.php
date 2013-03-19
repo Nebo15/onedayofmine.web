febd870cd1c9ce06f1250b7a49aa3b252d38419f
Stanislav Korchagin <korchasa@gmail.com>
2013-03-20 00:23:12 +0200

    Fix image paths

--- settings/images.conf.php
+++ settings/images.conf.php
@@ -1,23 +1,23 @@
 <?php
 
 $conf = array(
-  'save_path' => 'www',
+  'save_path' => 'www/users',
   'Day'    => array(
-    'path'      => 'users/days/:hash_:image_width.:file_extension',
+    'path'      => 'days/:hash_:image_width.:file_extension',
     'sizes'     => array(
                      array('width' => 266, 'height' => 266),
                      array('width' => 532, 'height' => 532),
                    ),
   ),
   'Moment' => array(
-    'path'      => 'users/days/:day_id/:hash_:image_width.:file_extension',
+    'path'      => 'days/:day_id/:hash_:image_width.:file_extension',
     'sizes'     => array(
                      array('width' => 266, 'height' => 266),
                      array('width' => 532, 'height' => 532),
                    ),
   ),
   'User'   => array(
-    'path'      => 'users/:id/:hash_:image_width.:file_extension',
+    'path'      => ':id/:hash_:image_width.:file_extension',
     'sizes'     => array(
                      array('width' => 36, 'height'  => 36),
                      array('width' => 72, 'height'  => 72),
--- tests/cases/unit/settings/images.conf.php
+++ tests/cases/unit/settings/images.conf.php
@@ -1,10 +1,34 @@
 <?php
-require('settings/images.conf.php');
-
-$conf['ImageableForTests'] = array(
-  'path'      => 'ImageableForTests_:id_:image_width.:file_extension',
-  'sizes'     => array(
-    array('width' => 266, 'height' => 266),
-    array('width' => 532, 'height' => 532),
-  ),
+$conf = array(
+	'save_path' => 'www',
+	'Day'    => array(
+		'path'      => 'users/days/:hash_:image_width.:file_extension',
+		'sizes'     => array(
+			array('width' => 266, 'height' => 266),
+			array('width' => 532, 'height' => 532),
+		),
+	),
+	'Moment' => array(
+		'path'      => 'users/days/:day_id/:hash_:image_width.:file_extension',
+		'sizes'     => array(
+			array('width' => 266, 'height' => 266),
+			array('width' => 532, 'height' => 532),
+		),
+	),
+	'User'   => array(
+		'path'      => 'users/:id/:hash_:image_width.:file_extension',
+		'sizes'     => array(
+			array('width' => 36, 'height'  => 36),
+			array('width' => 72, 'height'  => 72),
+			array('width' => 96, 'height'  => 96),
+			array('width' => 192, 'height' => 192),
+		),
+	),
+	'ImageableForTests' => array(
+		'path'      => 'ImageableForTests_:id_:image_width.:file_extension',
+		'sizes'     => array(
+			array('width' => 266, 'height' => 266),
+			array('width' => 532, 'height' => 532),
+		),
+	)
 );
