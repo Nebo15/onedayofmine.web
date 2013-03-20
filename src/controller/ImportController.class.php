160ca31999f1117e1ad8b1bd6be2219b06cfda9d
Stanislav Korchagin <korchasa@gmail.com>
2013-03-20 23:20:34 +0200

    Cache for external calls for photos. Ttl = 300 sec

--- src/controller/ImportController.class.php
+++ src/controller/ImportController.class.php
@@ -7,18 +7,33 @@ class ImportController extends BaseJsonController
 {
 	function doPhotos()
 	{
-		$service = new MergedPhotoSource($this->_getUser());
-		return $this->_answerOk(
-			$service->getPhotos(
-				$this->request->getInteger('from'),
-				$this->request->getInteger('to')
-			)
-		);
+		$from = $this->request->getInteger('from');
+		$to = $this->request->getInteger('to');
+
+		$cache = $this->toolkit->getCache('photo_sources');
+		$key = 'photos_'.$this->_getUser()->id.'_'.$from.'_'.$to;
+
+		if(!$result = $cache->get($key))
+		{
+			$result = $this->_answerOk((new MergedPhotoSource($this->_getUser()))->getPhotos($from, $to));
+			$cache->set($key, $result, 60 * 15);
+		}
+
+		return $result;
 	}
 
 	function doDays()
 	{
-		$service = new MergedPhotoSource($this->_getUser());
-		return $this->_answerOk($service->getDays($this->request->getInteger('from')));
+		$from = $this->request->getInteger('from');
+
+		$cache = $this->toolkit->getCache('photo_sources');
+		$key = 'days_'.$this->_getUser()->id.'_'.$from;
+
+		if(!$result = $cache->get($key))
+		{
+			$result = $this->_answerOk((new MergedPhotoSource($this->_getUser()))->getDays($from));
+			$cache->set($key, $result, 60 * 5);
+		}
+		return $result;
 	}
 }
