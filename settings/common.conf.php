8ba238483ea63dd7f2330bcea93c3f31be9d0598
Stanislav Korchagin <korchasa@gmail.com>
2013-03-22 01:01:45 +0200

    flickr settings for production

--- settings/common.conf.php
+++ settings/common.conf.php
@@ -14,7 +14,7 @@ $conf = array(
 	],
 
 	'flickr' => [
-		'key' => '7779fdde3c7525e0015d46694b6839d1',
-		'secret' => 'bdf98eeeba525020'
+		'key' => 'e33d877e38bfc9bf86e570fdb80b0dab',
+		'secret' => 'ae3f39047c2951f4'
 	]
 );
