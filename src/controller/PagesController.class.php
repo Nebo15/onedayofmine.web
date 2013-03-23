4e9f2bd7b13a63c80722ac1c187da34fb2d0a7e0
Andrew Dryga <andrew@dryga.com>
2013-03-23 12:24:33 +0200

    Fixed loader counter

--- src/controller/PagesController.class.php
+++ src/controller/PagesController.class.php
@@ -86,7 +86,7 @@ class PagesController extends lmbController
 
 	function doDaysDiscover()
 	{
-		$days_ratings = (new InterestCalculator())->getDaysRatings(null, null, 2);
+		$days_ratings = (new InterestCalculator())->getDaysRatings(null, null, 20);
 
 		$this->days = [];
 		foreach ($days_ratings as $day_rating)
