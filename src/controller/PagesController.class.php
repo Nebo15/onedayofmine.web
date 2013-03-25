2b35a798fd162db2ae86219acc956aad2bac4580
Andrew Dryga <andrew@dryga.com>
2013-03-26 01:05:58 +0200

    Fixed days_discover bug / Optimized days_discover

--- src/controller/PagesController.class.php
+++ src/controller/PagesController.class.php
@@ -88,17 +88,23 @@ class PagesController extends lmbController
 
 	function doDaysDiscover()
 	{
-		$days_ratings = (new InterestCalculator())->getDaysRatings(null, null, $this->lists_limit);
+		$top_days = array_map(function($day_rating) {
+      return $day_rating->getDay();
+    }, (array) (new InterestCalculator())->getDaysRatings(null, null, $this->lists_limit));
+
+    $top_days_exported = $this->toolkit->getExportHelper()->exportDayItems($top_days);
 
 		$this->days = [];
-		foreach ($days_ratings as $day_rating)
+		foreach ($top_days_exported as $day)
 		{
-			$item = $this->_toFlatArray($this->toolkit->getExportHelper()->exportDay($day_rating->getDay()));
+			$item = $this->_toFlatArray($day);
+
 			if(!$item['image_532'])
 				continue;
 			if(!$item['image_266'])
 				continue;
-			$this->days[] = $item;
+
+      $this->days[] = $item;
 		}
 	}
 
