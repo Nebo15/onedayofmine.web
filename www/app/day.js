d0dea2537a99005102daa6e1a2e5afd89415971c
Stanislav Korchagin <korchasa@gmail.com>
2013-03-29 20:41:23 +0200

    Fixed share fields. Title and image transfer by meta tags.

--- www/app/day.js
+++ www/app/day.js
@@ -1,35 +1,31 @@
-$(function() {
-  // Share toggle
-  var share_button = $('button#toggle_share');
-  share_button.click(function() {
-    if(!share_button.hasClass('disabled')) {
-      share_button.addClass('disabled');
+$(function () {
+	// Share toggle
+	var share_button = $('button#toggle_share');
+	share_button.click(function () {
+		var description = $('.final-description');
+		if (description.find('textarea').length > 0) {
+			description = description.find('textarea').text();
+		} else {
+			description = description.text();
+		}
 
-      var description = $('.final-description');
-      if(description.find('textarea').length > 0) {
-        description = description.find('textarea').text();
-      } else {
-        description = description.text();
-      }
+		var obj = {
+			method: 'feed',
+			link: window.location.href,
+			name: $('meta[property="og:title"]').prop('content'),
+			description: description,
+			picture: $('meta[property="og:image"]').prop('content')
+		};
 
-      var title = $('.day-title').text();
+		console.log(obj);
 
-      var obj = {
-          method: 'feed',
-          redirect_uri: 'http://'+Config.host+'/pages/{$#day->id}/day',
-          link: 'http://'+Config.host+'/pages/{$#day->id}/day',
-          name: title,
-          description: description
-      };
+		FB.ui(obj, function () {
+			share_button.addClass('btn-success');
+		});
+	});
 
-      FB.ui(obj, function() {
-        share_button.addClass('btn-success');
-      });
-    }
-  });
-
-  // Mobile nav comments
-  $('.mobile-nav #comments').click(function() {
-    $(document).scrollTo($('.comments'), 400);
-  });
+	// Mobile nav comments
+	$('.mobile-nav #comments').click(function () {
+		$(document).scrollTo($('.comments'), 400);
+	});
 });
