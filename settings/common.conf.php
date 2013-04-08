698c48bc0d8644e219d1d6f2c5351209282cd3f1
Stanislav Korchagin <korchasa@gmail.com>
2013-04-08 13:29:46 +0300

    secure s3 content

--- settings/common.conf.php
+++ settings/common.conf.php
@@ -4,7 +4,7 @@ $conf = array(
   'debug_enabled' => lmb_env_get('LIMB_APP_MODE') === 'devel',
   'default_news_count' => 100,
   'default_comments_count' => 3,
-  'static_host' => 'http://onedayofmine.s3.amazonaws.com/',
+  'static_host' => 'https://onedayofmine.s3.amazonaws.com/',
   'requests_log_enabled' => false,
 	'async_enabled' => true,
 
