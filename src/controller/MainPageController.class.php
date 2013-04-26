9d516f185b72c6fdab5a55c0ee454d1627ec238f
Stanislav Korchagin <korchasa@gmail.com>
2013-04-26 15:18:48 +0300

    Navigation changed

--- src/controller/MainPageController.class.php
+++ src/controller/MainPageController.class.php
@@ -24,60 +24,14 @@ class MainPageController extends WebAppController
 
 	function doDisplay()
 	{
-		if($this->toolkit->getUser())
-		{
-			$user  = $this->toolkit->getUser();
-			$news = $user->getNews()->paginate(0, $this->lists_limit);
-			$user->markAllNewsAsRead();
+		$days_ratings = (new InterestCalculator())->getDaysRatings(null, null, $this->lists_limit);
 
-			$this->news = $this->_mergeNews($news);
-			$this->news = $this->_toFlatArray($this->news);
+		$days = [];
+		foreach ($days_ratings as $day_rating)
+			$days[] = $day_rating->getDay();
 
-			$followers = $this->_getUser()->getFollowersUsers();
-			$this->followers = $this->_toFlatArray($this->toolkit->getExportHelper()->exportUserItems($followers));
-
-			$following = $this->_getUser()->getFollowingUsers();
-			$this->following = $this->_toFlatArray($this->toolkit->getExportHelper()->exportUserItems($following));
-
-			return;
-		}
-		else
-		{
-			$this->setTemplate('main_page/display_guest.phtml');
-		}
+		$this->days = $this->_toFlatArray($this->toolkit->getExportHelper()->exportDayItems($days));
 	}
 
-	protected function _mergeNews($news)
-	{
-		$news = $this->toolkit->getExportHelper()->exportNewsItems($news, true);
 
-		$result = [];
-		$current = null;
-		foreach($news as $news_item)
-		{
-			if($current && $current->text == $news_item->text)
-			{
-				if(property_exists($news_item, 'day') && $current->day != $news_item->day)
-					$current->days[] = $news_item->day;
-				if(property_exists($news_item, 'moment') && $current->moment != $news_item->moment)
-					$current->moments[] = $news_item->moment;
-				if(property_exists($news_item, 'day_comment') && $current->day_comment != $news_item->day_comment)
-					$current->day_comments[] = $news_item->day_comment;
-				if(property_exists($news_item, 'moment_comment') && $current->moment_comment != $news_item->moment_comment)
-					$current->moments_comments[] = $news_item->moment_comment;
-			}
-			else
-			{
-				if($current)
-					$result[] = $current;
-				$current = $news_item;
-				$current->days = $current->moments = $current->day_comments = $current->moment_comments = [];
-			}
-		}
-
-		if($current)
-			$result[] = $current;
-
-		return $result;
-	}
 }
