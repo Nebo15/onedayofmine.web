976d6dc04b8bad30d006494145a69d52aea75b49
Stanislav Korchagin <korchasa@gmail.com>
2013-05-05 16:44:11 +0300

    Редирект на профиль в instagram из админки

--- src/controller/AdminSiteUserController.class.php
+++ src/controller/AdminSiteUserController.class.php
@@ -1,9 +1,16 @@
 <?php
 lmb_require('limb/cms/src/controller/lmbAdminObjectController.class.php');
 lmb_require('src/model/User.class.php');
+lmb_require('src/service/social_photo_source/InstagramPhotoSource.class.php');
 
 class AdminSiteUserController extends lmbAdminObjectController
 {
   protected $_object_class_name = 'User';
 
-  }
+	function doRedirectToInstagram()
+	{
+		$user = User::findById($this->request->getInteger('id'));
+		$this->url = 'http://instagram.com/'.(new InstagramPhotoSource($user))->getUserInfo()['name'];
+		$this->setTemplate('admin_site_user/redirect.phtml');
+	}
+}
