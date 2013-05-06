725f00b8eb13fed3b4bc30c7f1c69e1b830fe077
Stanislav Korchagin <korchasa@gmail.com>
2013-05-06 18:49:34 +0300

    Добавил поле date в days/journal

--- src/controller/DaysController.class.php
+++ src/controller/DaysController.class.php
@@ -255,8 +255,11 @@ class DaysController extends BaseJsonController
 	function doJournal()
 	{
 		list($from, $to, $limit) = $this->_getFromToLimitations();
-		$days = DayJournalRecord::findDaysWithLimitation($from, $to, $limit);
-		$days = $this->toolkit->getExportHelper()->exportDayItems($days);
+		$days = $this->toolkit->getExportHelper()->exportDayItems(
+			DayJournalRecord::findDaysWithLimitation($from, $to, $limit)
+		);
+		foreach($days as $i => $day)
+			$days[$i]->date = date('Y-m-d', $day->utime);
 		return $this->_answerOk($days);
 	}
 
