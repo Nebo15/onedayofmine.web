8d72e7e76ff0ed3892f95134e2fad295c6cab792
Stanislav Korchagin <korchasa@gmail.com>
2013-05-07 13:51:11 +0300

    Добавил указание позиции момента в импорте

--- www/app/day.import.js
+++ www/app/day.import.js
@@ -9,9 +9,9 @@ $(function() {
       $import_btn.addClass('disabled');
 
       var messages = [
-        "Calculating white pixels…",
-        "It's better to dont know, trust me",
-        "Closed for dinner",
+        //"Calculating white pixels…",
+        //"It's better to don't know, trust me",
+        //"Closed for dinner",
         "It's gonna be legen… wait for it…"
       ];
 
@@ -24,34 +24,34 @@ $(function() {
       var $progress_message = $body.find('.text');
       var $progress_bar = $progress.find('.bar');
 
-      function setProgress(percents, callback) {
+      function setProgress(percents, message, callback) {
         $progress_bar.css('width', percents + '%');
+				$progress_message.text(message);
 
         if(percents === 100) {
           setTimeout(function() {
             $progress.slideUp(animations_speed, callback);
           }, 500);
         } else if($progress.css('display') == 'none') {
-          $progress_message.text(messages[Math.floor(Math.random()*messages.length)]);
           $progress.slideDown(animations_speed);
         }
       }
 
       $('body').append($modal);
       $modal.modal('show');
-      setProgress(0);
+      setProgress(0, "Let the carnage begin!");
 
       API.post('days/start', { title: day_data.title, type:'Working day' }).success(function(resp) {
         var day = resp.data.result;
         var step_percentage = 100/(day_data.moments.length+1);
-        setProgress(step_percentage);
-
+        setProgress(step_percentage, messages[Math.floor(Math.random()*messages.length)]);
         var requests = [];
         var finished_count = 1;
 
         $.each(day_data.moments, function(index, moment) {
           var moment_data = {
             time:         day_data.date + 'T' + moment.time + ':' + '00' + Tools.getTimezone(),
+						position:     index,
             image_url:    moment.image_532,
             description:  moment.description
           };
@@ -60,7 +60,7 @@ $(function() {
 
           moment_create_request.success(function (response) {
             finished_count++;
-            setProgress(finished_count*step_percentage+1);
+            setProgress(finished_count*step_percentage+1, messages[Math.floor(Math.random()*messages.length)]);
           });
 
           requests.push(moment_create_request.send());
