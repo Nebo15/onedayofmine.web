627996394ba387e9f5d59e8aba1b05efb6572286
Stanislav Korchagin <korchasa@gmail.com>
2013-05-08 12:09:38 +0300

    Свой AdminObjectController под новый ActiveRecord

--- src/controller/AdminSiteUserController.class.php
+++ src/controller/AdminSiteUserController.class.php
@@ -1,11 +1,12 @@
 <?php
-lmb_require('limb/cms/src/controller/lmbAdminObjectController.class.php');
+lmb_require('src/controller/AdminObjectController.class.php');
 lmb_require('src/model/User.class.php');
 lmb_require('src/service/social_photo_source/InstagramPhotoSource.class.php');
 
-class AdminSiteUserController extends lmbAdminObjectController
+class AdminSiteUserController extends AdminObjectController
 {
   protected $_object_class_name = 'User';
+	protected $_default_sort = ['id' => 'desc'];
 
 	function doRedirectToInstagram()
 	{
