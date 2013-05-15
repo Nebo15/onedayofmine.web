d228a3b3ae8455967c2d02e293087a317f02d6ed
Stanislav Korchagin <korchasa@gmail.com>
2013-05-15 15:28:46 +0300

    Окружение в vagrant

--- init/vagrant/bootstrap.sh
+++ init/vagrant/bootstrap.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y nginx php5-fpm 
 rm -rf /var/www
-ln -fs /vagrant/odom /var/www
+mkdir -p /www/
+ln -fs /vagrant/odom/ /www/onedayofmine
+chown -R www-data:www-data /www/onedayofmine/*
+
+# Need conditionals around `mesg n` so that Chef doesn't throw
+# `stdin: not a tty`
+sed -i '$d' /root/.profile
+cat << 'EOH' >> /root/.profile
+if `tty -s`; then
+  mesg n
+fi
+EOH
+
+echo '#!/bin/sh' > /bin/puppet-update.sh
+echo "cd /tmp/vagrant-puppet/manifests" >> /bin/puppet-update.sh
+echo "/opt/ruby/bin/puppet apply --modulepath  '/etc/puppet/modules:/tmp/vagrant-puppet/modules-0' site.pp --detailed-exitcodes" >> /bin/puppet-update.sh
+chmod 755 /bin/puppet-update.sh
+
+echo '#!/bin/sh' > /bin/puppet-debug-update.sh
+echo "cd /tmp/vagrant-puppet/manifests" >> /bin/puppet-debug-update.sh
+echo "/opt/ruby/bin/puppet apply --verbose --debug --modulepath  '/etc/puppet/modules:/tmp/vagrant-puppet/modules-0' site.pp --detailed-exitcodes" >> /bin/puppet-debug-update.sh
+chmod 755 /bin/puppet-debug-update.sh
---
+++ init/vagrant/manifests/site.pp
@@ -0,0 +1,56 @@
+group { 'puppet':
+	ensure => present,
+}
+
+host { 'onedayofmine.dev':
+    ip => '127.0.0.1',
+    host_aliases => 'api.onedayofmine.dev',
+}
+
+apt::ppa { "ppa:ondrej/php5": }
+
+exec { 'apt-get update':
+	command => '/usr/bin/apt-get update',
+}
+
+package { ['nginx', 'mysql', 'php5-fpm', 'php5-cli', 'php5-curl', 'php5-imagick', 'php5-memcached', 'php5-mysqlnd']:
+	ensure => present,
+	require => Exec['apt-get update'],
+}
+
+service { 'nginx':
+	ensure => running,
+	require => Package['nginx'],
+}
+
+service { 'php5-fpm':
+	ensure => running,
+	require => Package['php5-fpm'],
+}
+
+file { 'vagrant-nginx':
+	path => '/etc/nginx/sites-available/vagrant',
+	ensure => file,
+  replace => true,
+	require => Package['nginx'],
+	source => '/www/onedayofmine/settings/third-party/front/nginx/oneday-dev.conf',
+  notify => Service['nginx'],
+}
+
+file { 'default-nginx-disable':
+	path => '/etc/nginx/sites-enabled/default',
+	ensure => absent,
+	require => Package['nginx'],
+}
+
+file { 'vagrant-nginx-enable':
+	path => '/etc/nginx/sites-enabled/vagrant',
+	target => '/etc/nginx/sites-available/vagrant',
+	ensure => link,
+	notify => Service['nginx'],
+	require => [
+		File['vagrant-nginx'],
+		File['default-nginx-disable'],
+	],
+}
+
