89f54f08561191410d00c84c25ffdab1f11d4f47
Stanislav Korchagin <korchasa@gmail.com>
2013-06-02 11:43:20 +0300

    Поправил коммит хук

--- cli/git/git_hook-commit_msg-check_ticket.sh
+++ cli/git/git_hook-commit_msg-check_ticket.sh
@@ -2,10 +2,7 @@
 
 echo " >>> Running pre-commit hook: search ticket link"
 
-if [[ "$1" =~ '#' ]]
-then
-	exit 0;
-else
-	echo "Ticket number not found! Use \"#\", Luck!";
-	exit 1;
-fi
+test "" != "$(grep '#' $1)" || {
+	echo >&2 "Ticket number not found! Use \"#\", Luck!";
+	exit 1
+}
