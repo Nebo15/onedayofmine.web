e62b1667aa91b2dc220b869abd6311b83799c341
Stanislav Korchagin <korchasa@gmail.com>
2013-06-04 17:35:38 +0300

    #9999 Fixed rss content type and charset

--- src/controller/RssController.class.php
+++ src/controller/RssController.class.php
@@ -11,6 +11,7 @@ class RssController extends WebAppController
 {
 	function doJournal()
 	{
+		$this->response->addHeader('Content-Type: application/rss+xml; charset=utf-8');
 		$this->build_time = date('r');
 		$this->items = DayJournalRecord::findDaysWithLimitation(null, null, 20);
 	}
