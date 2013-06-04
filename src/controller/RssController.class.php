64ec9c8a0d19f65b78feb7aba2d32afd564f8637
Stanislav Korchagin <korchasa@gmail.com>
2013-06-04 17:22:02 +0300

    #9999 Journal rss

---
+++ src/controller/RssController.class.php
@@ -0,0 +1,17 @@
+<?php
+lmb_require('src/controller/WebAppController.class.php');
+lmb_require('src/controller/DaysController.class.php');
+lmb_require('src/service/InterestCalculator.class.php');
+lmb_require('tests/src/toolkit/odTestsTools.class.php');
+lmb_require('src/Json.class.php');
+lmb_require('src/model/DayJournalRecord.class.php');
+lmb_require('src/model/Day.class.php');
+
+class RssController extends WebAppController
+{
+	function doJournal()
+	{
+		$this->build_time = date('r');
+		$this->items = DayJournalRecord::findDaysWithLimitation(null, null, 20);
+	}
+}
