7d80e0070c100a82e4897e93c1a67f2dc6c53b1d
Stanislav Korchagin <korchasa@gmail.com>
2013-06-10 11:10:08 +0300

    #9999 trim для всех get параметров

--- src/controller/BaseJsonController.class.php
+++ src/controller/BaseJsonController.class.php
@@ -130,7 +130,7 @@ abstract class BaseJsonController extends lmbController
   {
     foreach($properties as $property)
       if($this->request->get($property))
-        $item->set($property, $this->request->get($property));
+        $item->set($property, trim($this->request->get($property)));
 
     foreach ($raw_properties as $key => $value)
       $item->set($key, trim($value));
