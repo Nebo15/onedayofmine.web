79f11dba83ee839e4612651ee5e3ebd9c7a588ab
Andrew Dryga <andrew@dryga.com>
2013-06-15 02:15:01 +0300

    Instert plugin style after DOM-ready

--- www/lib/vendor/jquery.textarea.autoresize.js
+++ www/lib/vendor/jquery.textarea.autoresize.js
@@ -1,3 +1,26 @@
+/*
+ * jQuery Textarea.Autoresize
+ * https://github.com/AndrewDryga/jQuery.Textarea.Autoresize
+ *
+ * This plugin resizes textarea height to match it's content height.
+ *
+ * Usage:
+ * <code>$('textarea').autoresize(params_object);</code>
+ *
+ * Params can also be passed via data-api:
+ * <code><textarea data-default-height="min" data-animated="false"></textarea></code>
+ *
+ * Available params:
+ * - minHeight: minimal height for textarea, default is textarea height.
+ * - maxHeight: maximal height for textarea, default is false (unlimited). If textarea content is bigger than this value, then scrollbar appears.
+ * - defaultHeight: height that will be set to textarea when it loose focus. Default if false (turned off).
+ * - animated: animation for focus loose/restore (works only width non-false defaultHeight values). Default is true.
+ * - onResize: callback function, called each time plugin resizes textarea.
+ *
+ * Author: Andrew Dryga <andrew@dryga.com> <http://dryga.com>
+ * License: MIT
+ */
+
 (function($) {
   "use strict";
 
@@ -152,6 +175,8 @@
     });
   };
 
-  // Instert plugin styles
-  $('html > head').append($('<style>.autoresize-mirror { display: none; word-wrap: break-word; white-space: pre-wrap; } .autoresize { resize: none; }</style>'));
+  $(function() {
+    // Instert plugin styles
+    $('html > head').append($('<style>.autoresize-mirror { display: none; word-wrap: break-word; white-space: pre-wrap; } .autoresize { resize: none; }</style>'));
+  });
 })(jQuery);
