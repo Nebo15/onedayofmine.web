5cf6981eda31131998d09a68176adeb5189af9ad
Stanislav Korchagin <korchasa@gmail.com>
2013-06-20 20:48:32 +0300

    #9999. Сессии польностью переложены в cache. Для включения/выключения нужно править cache.conf.php session_cache_dsn

---
+++ tests/cases/unit/settings/cache.conf.php
@@ -0,0 +1,10 @@
+<?php
+
+$conf = array();
+$conf['cache_enabled'] = true;
+$conf['default_cache_dsn'] = "fake:///";
+$conf['fb_cache_dsn'] = "fake:///";
+$conf['session_cache_dsn'] = "fake:///";
+$conf['db_info_cache_dsn'] = "fake:///";
+$conf['photo_sources_cache_dsn'] = "fake:///";
+$conf['i18n_cache_dsn'] = "fake:///";
